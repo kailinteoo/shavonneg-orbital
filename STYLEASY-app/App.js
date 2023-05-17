9a20958c9b2f07a2a910bce48a93b4561fe3e401
Kailin Teo <e0959354@u.nus.edu>
2023-05-17 22:49:50 +0800

    firebase and different pages

--- STYLEASY-app/App.js
+++ STYLEASY-app/App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Chat from "./pages/Chat";
 
-export default function App() {
+// SEND THE USER TO LOGINPAGE OR CHATPAGE OR WTV PAGE
+const Stack = createStackNavigator();
+
+function ChatStack () {
   return (
-    <View style={styles.container}>
-      <Text>HI SMARTCOOKIES</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Stack.Navigator>
+      <Stack.Page name = "Chat" component={Chat} />
+    </Stack.Navigator>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// IF YOU WANT TO ADD MORE NAVIGATION, YOU CAN ADD IT INSIDE THIS NAVIGATION CONTAINER
+function RootNavigator () {
+  return (
+    <NavigationContainer>
+      <ChatStack />
+    </NavigationContainer>
+  )
+}
+
+export default function App() {
+  return <RootNavigator />
+}
+
+
---
+++ STYLEASY-app/config/pages/Chat.js
@@ -0,0 +1,3 @@
+import React from "react";
+import { GiftedChat } from "react-native-gifted-chat";
+
