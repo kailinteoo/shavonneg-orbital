f3945171d2757988ab7f34606558b9c378acad28
shavonneg <e0967851@u.nus.edu>
2023-05-18 23:33:24 +0800

    Update Login.js

--- STYLEASY-app/screens/Login.js
+++ STYLEASY-app/screens/Login.js
@@ -1,51 +1,45 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput, Image, SafeAreaview, TouchableOpacity, StatusBar, Alert } from "react-native";
-import { signinWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../config/Firebase";
-//const backImage = require("../assets/backImage .png");
+import {Ionicons} from '@expo/vector-icons'
 
-export default function Login({ navigation }) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+export default class LoginScreen extends React.Component { 
+    state = {
+        name: ""
+    }
 
-    const onHandleLogin = () => {
-        if (email !== "" && password !== "") {
-            signInWithEna1lAndPassword(auth, email, password)
-             .then(() => console. log("Login success"))
-             .catch((err) - Alert.alert ("Login error", err.nessage));
-        }
-    };
+    continue = () => {
+        this.props.navigation.navigate("Chat", {name: this.state.name})
+    }
 
-    return (
-        <View style={styles.container}>
-            <Image source={backImage} style={styles.backImage} />
-            <View style={styles.whiteSheet} />
-            <SafeAreaview style={styles.form} />
-                <Text style={styles.title}>Login</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Enter email"
-                    autoCapitalize="none"
-                    keyboardType="email-address"
-                    autoFocus={true}
-                    value={email}
-                    onChangeText={(text) => setEmail(text)}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="Enter password"
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    secureTextEntry={true}
-                    textContentType="password"
-                    value={email}
-                    onChangeText={(text) => setPassword(text)}
-                />                
-        </View>
-    )
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={{ marginHorizontal: 32}}>
+                    <Text style={styles.title}>Username</Text>
+                    <TextInput 
+                        style={styles.input} 
+                        placeholder="DesignIntoCode" 
+                        onChangeText={name => { 
+                            this.setState({ name });
+                        }} 
+                        value={this.state.name}
+                    />
+                </View>
+                <View style={{alignItems: "flex-end", marginTop: 64}}>
+                    <TouchableOpacity style={styles.continue} onPress={this.continue}>
+                        <Ionicons 
+                            name="md-arrow-round-forward" 
+                            size={24}
+                            color="#FFF"
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
+    }
 }
 
-const styles = StyleSheet.createl({
+const styles = StyleSheet.create({
     container: { 
         flex: 1, 
         backgroundColor: "#fff",
@@ -93,4 +87,12 @@ const styles = StyleSheet.createl({
         alignItens: 'center', 
         marginTop: 40,
     },  
+    continue: {
+        width:70, 
+        height:70,
+        borderRadius: 70 / 2,
+        backgroundColor: "#9075E3",
+        alignItems: "center",
+        justifyContent: "center"
+    }
 }); 
