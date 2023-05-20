0864a61c9dd5e91fc289940bdae177d407c05462
Kailin Teo <e0959354@u.nus.edu>
2023-05-21 01:51:04 +0800

    homepage(halfway)

--- STYLEASY-app/App.js
+++ STYLEASY-app/App.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { View, ActivityIndicator } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { onAuthStateChanged } from "firebase/auth";
 
 import Chat from "./screens/Chat";
 import Login from "./screens/Login"
 import Signup from "./screens/Signup"
+import Home from "./screens/Home";
 
 
 // SEND THE USER TO LOGINPAGE OR CHATPAGE OR WTV PAGE
 const Stack = createStackNavigator();
+const onAuthStateChanged = createContext({});
 
-function LoginStack () {
+const AuthenticatedUserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name = "Signup" component = {Signup} />
+    <AuthenticatedUserContext.Provider value={[user, setUser]}>
+      {children}
+    </AuthenticatedUserContext.Provider>
+  )
+}
+
+function ChatStack () {
+  return (
+    <Stack.Navigator defaultScreenOptions={Home}>
+      <Stack.Screen name = "Home" component = {Home} />
+      <Stack.Screen name = "Chat" component = {Chat} />
+    </Stack.Navigator>
+  )
+}
+
+function AuthStack () {
+  return (
+    <Stack.Navigator defaultScreenOptions={Login}>
       <Stack.Screen name = "Login" component = {Login} />
+      <Stack.Screen name = "Signup" component = {Signup} />
     </Stack.Navigator>
   )
 }
@@ -22,9 +44,28 @@ function LoginStack () {
 
 // IF YOU WANT TO ADD MORE NAVIGATION, YOU CAN ADD IT INSIDE THIS NAVIGATION CONTAINER
 function RootNavigator () {
+  const { user, setUser } = useContext(AuthenticatedUserContext);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth,
+      async authenticatedUser => {
+        authenticatedUser ? setUser(authenticatedUser) : setUser(null);
+        setLoading(false);
+      }
+    );
+    return () => unsubscribe();
+  }, [user]);
+  if(loading) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
+
   return (
     <NavigationContainer>
-      <LoginStack />
+      { user ? <ChatStack /> : <AuthStack />}
     </NavigationContainer>
   )
 }
