5a8efc6d162f788a420cd9931cc7c4c6a6966192
Kailin Teo <e0959354@u.nus.edu>
2023-05-28 18:04:39 +0800

    learnmore

--- STYLEASY-app/screens/LearnMore.js
+++ STYLEASY-app/screens/LearnMore.js
@@ -37,8 +37,15 @@ const LearnMore = () => {
             <View style = {styles.container}>
                 <ScrollView>
                     <Text style={styles.HeaderOne}>Who Are We</Text>  
-                    <Text style={styles.HeaderTwo}>We are STYLEASY who hope to make the process of choosing an outfit quick, create a place where we can keep track of all our clothing items, and integrate a sense of community by connecting like-minded individuals who have similar fashion preferences.
- </Text>  
+                    <Text style={styles.HeaderTwo}>We are a team of two software engineers, working together to make our app come to life. We made this app in hopes that we can make styling clothes an easy and brainless process, reducing the time needed to try different outfit combinations to find the best fit for the occasion.</Text>  
+                    <Text style={styles.HeaderThree}>How to use our app?</Text>
+                    <Text style={styles.HeaderFour}>Our app consists of three major components</Text>
+                    <Text style={styles.HeaderFive}>1. Wardrobe</Text>
+                    <Text style={styles.HeaderTwo}>Inside your wardrobe, you will be able to view your entire inventory of clothes, as well as look at your saved outfits to refer them for future use.</Text>
+                    <Text style={styles.HeaderFive}>2. Fitting Room</Text>
+                    <Text style={styles.HeaderTwo}>Here, we let you personalise your outfits without having to try them on!</Text>
+                    <Text style={styles.HeaderFive}>3. Community </Text>
+                    <Text style={styles.HeaderTwo}>Our app lets you talk to your friends, family, or even like-minded users of our app to bring together a community of fashion enthusiasts.</Text>
                 </ScrollView>
             </View>
     );
@@ -54,7 +61,7 @@ export default LearnMore;
             backgroundColor: '#fdf5e6',
         },
         HeaderOne: {
-            marginTop: 20, 
+            marginTop: 50, 
             marginLeft: 60,
             color:'black', 
             fontWeight: 'bold', 
@@ -62,11 +69,37 @@ export default LearnMore;
         },
         HeaderTwo: {
             textAlign: 'justify',
-            marginTop: 20,
+            marginTop: 10,
             marginRight: 30,
             color:'black',
             fontSize:12,
             width: 300,
-
-        }
+        },
+        HeaderThree: {
+            textAlign: 'center',
+            marginTop: 40,
+            marginRight: 40,
+            color:'black',
+            fontWeight: 'bold', 
+            fontSize:30,
+            width: 300,
+        },
+        HeaderFour: {
+            textAlign: 'justify',
+            marginTop: 10,
+            marginLeft: 8,
+            color:'black',
+            fontWeight: 'bold', 
+            fontSize:12,
+            width: 300,
+        },
+        HeaderFive: {
+            textAlign: 'justify',
+            marginTop: 20,
+            marginLeft: 20,
+            color:'black',
+            fontWeight: 'bold', 
+            fontSize:20,
+            width: 300,
+        },
     });
