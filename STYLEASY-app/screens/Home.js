e6df38b3e42f23cf87d4f7cd7e57664021733d90
Kailin Teo <e0959354@u.nus.edu>
2023-06-08 01:00:40 +0800

    collection UI update

--- STYLEASY-app/screens/Home.js
+++ STYLEASY-app/screens/Home.js
@@ -49,6 +49,10 @@ const Home = () => {
     // Calculate the font size for the label based on the screen width
     const labelFontSize = windowWidth * 0.03;
 
+    // Calculate the font size for the button text based on the screen width
+    const textFontSize = windowWidth * 0.05;
+    const textHeight = windowHeight * 0.1;
+
     // Calculate the margin bottom based on the screen height
     const marginBottom = windowHeight * 0.1; // Adjust the percentage as desired
 
@@ -61,7 +65,7 @@ const Home = () => {
             </View>
 
             <View style={styles.middleContainer}>
-                <TouchableOpacity style={styles.LearnMoreButton}>
+                <TouchableOpacity style={styles.LearnMoreButton} onPress={() => navigation.navigate("LearnMore")}>
                     <Text style={styles.LearnMoreButtonText}>Learn More</Text>
                 </TouchableOpacity>
             </View>
@@ -69,7 +73,7 @@ const Home = () => {
             <View style={styles.bottomContainer}>
                 <TouchableOpacity style={[styles.Button, {width: buttonWidth, height: windowHeight * 0.15}]} onPress={() => navigation.navigate("Collection")}>
                     <ImageBackground source={require('../assets/collection.png')} style={styles.buttonBackground}>
-                        <Text style={styles.buttonText}>COLLECTION</Text>
+                        <Text style={[styles.buttonText, { fontSize: textFontSize, marginTop: textHeight }]}>COLLECTION</Text>
                     </ImageBackground>
                 </TouchableOpacity>
 
@@ -77,7 +81,7 @@ const Home = () => {
 
                 <TouchableOpacity style={[styles.Button, {width: buttonWidth, height: windowHeight * 0.15}]} onPress={() => navigation.navigate("FittingRoom")}>
                     <ImageBackground source={require('../assets/FittingRoom.png')} style={styles.buttonBackground}>
-                        <Text style={styles.buttonText}>FITTING ROOM</Text>
+                        <Text style={[styles.buttonText, { fontSize: textFontSize, marginTop: textHeight }]}>FITTING ROOM</Text>
                     </ImageBackground>
                 </TouchableOpacity>
 
@@ -85,7 +89,7 @@ const Home = () => {
 
                 <TouchableOpacity style={[styles.Button, {width: buttonWidth, height: windowHeight * 0.15}]} onPress={() => navigation.navigate("Community")}>
                     <ImageBackground source={require('../assets/community.png')} style={styles.buttonBackground}>
-                        <Text style={styles.buttonText}>COMMUNITY</Text>
+                        <Text style={[styles.buttonText, { fontSize: textFontSize, marginTop: textHeight }]}>COMMUNITY</Text>
                     </ImageBackground>
                 </TouchableOpacity>
             </View>
@@ -147,6 +151,7 @@ const Home = () => {
           buttonText: {
             color: 'white',
             fontWeight: 'bold',
+            alignSelf: "center",
           },
           gap: {
             height: 20, // Adjust the height as needed
--- STYLEASY-app/screens/LearnMore.js
+++ STYLEASY-app/screens/LearnMore.js
@@ -1,111 +1,117 @@
-import React, {
-    useState,
-    useEffect,
-    useLayoutEffect,
-    useCallback
-} from "react";
-import { View, TouchableOpacity, Text, Image, StyleSheet, ImageBackground, ScrollView, SafeAreaView} from "react-native";
+import React, { useState, useEffect, useLayoutEffect, useCallback } from 'react';
+import { View, TouchableOpacity, Text, Image, StyleSheet, StatusBar, ImageBackground, SafeAreaView, ScrollView, useWindowDimensions } from 'react-native';
 import { signOut } from 'firebase/auth';
 import { auth, database } from '../config/firebase';
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation } from '@react-navigation/native';
 import { AntDesign } from '@expo/vector-icons';
-import colors from "../colors";
+import colors from '../colors';
 
 const LearnMore = () => {
+  const navigation = useNavigation();
+  const onSignOut = () => {
+    signOut(auth).catch((error) => console.log(error));
+  };
 
-    const navigation = useNavigation();
-    const onSignOut = () => {
-        signOut(auth).catch(error => console.log(error));
-    };
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          style={{
+            marginRight: 10,
+          }}
+          onPress={onSignOut}
+        >
+          <AntDesign name="logout" size={24} color={colors.black} style={{ marginRight: 10 }} />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerRight: () => (
-                <TouchableOpacity
-                    style={{
-                        marginRight: 10
-                    }}
-                    onPress={onSignOut}
-                >
-                    <AntDesign name="logout" size={24} color={colors.black} style={{marginRight: 10}}/>
-                </TouchableOpacity>
-              )
-            });
-          }, [navigation]);     
-          
-          return (
-            <View style = {styles.container}>
-                <SafeAreaView style={styles.form}>
-                <ScrollView>
-                    <Text style={styles.HeaderOne}>Who Are We</Text>  
-                    <Text style={styles.HeaderTwo}>We are a team of two software engineers, working together to make our app come to life. We made this app in hopes that we can make styling clothes an easy and brainless process, reducing the time needed to try different outfit combinations to find the best fit for the occasion.</Text>  
-                    <Text style={styles.HeaderThree}>How to use our app?</Text>
-                    <Text style={styles.HeaderFour}>Our app consists of three major components</Text>
-                    <Text style={styles.HeaderFive}>1. Wardrobe</Text>
-                    <Text style={styles.HeaderTwo}>Inside your wardrobe, you will be able to view your entire inventory of clothes, as well as look at your saved outfits to refer them for future use.</Text>
-                    <Text style={styles.HeaderFive}>2. Fitting Room</Text>
-                    <Text style={styles.HeaderTwo}>Here, we let you personalise your outfits without having to try them on!</Text>
-                    <Text style={styles.HeaderFive}>3. Community </Text>
-                    <Text style={styles.HeaderTwo}>Our app lets you talk to your friends, family, or even like-minded users of our app to bring together a community of fashion enthusiasts.</Text>
-                </ScrollView>
-                </SafeAreaView>
-            </View>
-    );
+  const { width: windowWidth, height: windowHeight } = useWindowDimensions();
+
+  const labelFontSize = windowWidth * 0.03;
+
+  return (
+    <View style={styles.container}>
+      <SafeAreaView style={styles.form}>
+        <ScrollView>
+          <Text style={[styles.HeaderOne, { fontSize: labelFontSize }]}>Who Are We</Text>
+          <View style={styles.gap} />
+          <Text style={[styles.HeaderTwo, { fontSize: labelFontSize }]}>
+            We are a team of two software engineers, working together to make our app come to life. We made this app in hopes that we can make
+            styling clothes an easy and brainless process, reducing the time needed to try different outfit combinations to find the best fit
+            for the occasion.
+          </Text>
+          <View style={styles.gap} />
+          <Text style={[styles.HeaderThree, { fontSize: labelFontSize }]}>How to use our app?</Text>
+          <View style={styles.gap} />
+          <Text style={[styles.HeaderFour, { fontSize: labelFontSize }]}>Our app consists of three major components</Text>
+          <View style={styles.gap} />
+          <Text style={[styles.HeaderFive, { fontSize: labelFontSize }]}>1. Wardrobe</Text>
+          <Text style={[styles.HeaderTwo, { fontSize: labelFontSize }]}>
+            Inside your wardrobe, you will be able to view your entire inventory of clothes, as well as look at your saved outfits to refer
+            them for future use.
+          </Text>
+          <View style={styles.gap} />
+          <Text style={[styles.HeaderFive, { fontSize: labelFontSize }]}>2. Fitting Room</Text>
+          <Text style={[styles.HeaderTwo, { fontSize: labelFontSize }]}>Here, we let you personalise your outfits without having to try them on!</Text>
+          <View style={styles.gap} />
+          <Text style={[styles.HeaderFive, { fontSize: labelFontSize }]}>3. Community </Text>
+          <Text style={[styles.HeaderTwo, { fontSize: labelFontSize }]}>
+            Our app lets you talk to your friends, family, or even like-minded users of our app to bring together a community of fashion
+            enthusiasts.
+          </Text>
+        </ScrollView>
+      </SafeAreaView>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#fdf5e6',
+  },
+  HeaderOne: {
+    color: 'black',
+    fontWeight: 'bold',
+    alignSelf: "center",
+  },
+  HeaderTwo: {
+    textAlign: 'justify',
+    color: 'black',
+    fontSize: 12,
+  },
+  HeaderThree: {
+    textAlign: 'center',
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  HeaderFour: {
+    textAlign: 'justify',
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 12,
+    alignSelf: "center",
+  },
+  HeaderFive: {
+    textAlign: 'justify',
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 60,
+    marginVertical: 50,
+  },
+  gap: {
+    height: 30, // Adjust the height as needed
+  },
+});
 
 export default LearnMore;
 
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            alignItems: 'center',
-            backgroundColor: '#fdf5e6',
-        },
-        HeaderOne: {
-            marginTop: 55, 
-            marginLeft: "20%",
-            color:'black', 
-            fontWeight: 'bold', 
-            fontSize:30,
-        },
-        HeaderTwo: {
-            textAlign: 'justify',
-            marginTop: 10,
-            marginRight: 30,
-            color:'black',
-            fontSize:12,
-            width: 300,
-        },
-        HeaderThree: {
-            textAlign: 'center',
-            marginTop: 40,
-            marginRight: 40,
-            color:'black',
-            fontWeight: 'bold', 
-            fontSize:30,
-            width: 300,
-        },
-        HeaderFour: {
-            textAlign: 'justify',
-            marginTop: 10,
-            marginLeft: 8,
-            color:'black',
-            fontWeight: 'bold', 
-            fontSize:12,
-            width: 300,
-        },
-        HeaderFive: {
-            textAlign: 'justify',
-            marginTop: 20,
-            marginLeft: 20,
-            color:'black',
-            fontWeight: 'bold', 
-            fontSize:20,
-            width: 300,
-        },
-        form: {
-            flex: 1, 
-            justifyContent: 'center', 
-            marginHorizontal: 30,
-        },
-    });
