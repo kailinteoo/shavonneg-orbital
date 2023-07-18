9df40e99191ebe091d41b008d52b8d867f96185c
shavonneg <e0967851@u.nus.edu>
2023-07-18 23:31:53 +0800

    camera should work now

--- STYLEASY-app/screens/Camera.js
+++ STYLEASY-app/screens/Camera.js
@@ -1,15 +1,12 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 import { Camera } from 'expo-camera';
-import { useState, useRef, useEffect } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Alert } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { collection, addDoc } from 'firebase/firestore';
+import { auth, database } from '../config/firebase';
 
-
-
-export default function CameraScreen() {
+const CameraScreen = () => {
   const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
-  const [capturedPhoto, setCapturedPhoto] = useState(null);
   const cameraRef = useRef(null);
   const navigation = useNavigation();
 
@@ -20,47 +17,58 @@ export default function CameraScreen() {
     })();
   }, []);
 
-  const flipCamera = () => {
-    setType(
-      type === Camera.Constants.Type.back
-        ? Camera.Constants.Type.front
-        : Camera.Constants.Type.back
-    );
-  };
-
   const takePicture = async () => {
     if (cameraRef.current) {
-      const { uri } = await cameraRef.current.takePictureAsync();
-      setCapturedPhoto(uri);
-      showSaveConfirmation();
+      try {
+        const { uri } = await cameraRef.current.takePictureAsync();
+        showSaveConfirmation(uri);
+      } catch (error) {
+        console.log('Error taking picture:', error);
+      }
     }
   };
 
-  const showSaveConfirmation = () => {
+  const showSaveConfirmation = (photoUri) => {
     Alert.alert(
       'Save to Collection',
-      'Do you want to save this photo to your collection?',
+      'Which collection do you want to save this photo to?',
       [
         {
           text: 'Cancel',
           style: 'cancel',
-          onPress: resetPhoto,
         },
         {
-          text: 'Save',
-          onPress: saveToCollection,
+          text: 'Tops',
+          onPress: () => saveToCollection('tops', photoUri),
+        },
+        {
+          text: 'Bottoms',
+          onPress: () => saveToCollection('bottoms', photoUri),
+        },
+        {
+          text: 'Shoes',
+          onPress: () => saveToCollection('shoes', photoUri),
         },
       ]
     );
   };
 
-  const resetPhoto = () => {
-    setCapturedPhoto(null);
+  const saveToCollection = async (collectionName, photoUri) => {
+    const currentUser = auth.currentUser;
+    if (currentUser) {
+      const collectionRef = collection(database, 'users', currentUser.uid, collectionName);
+      try {
+        await addDoc(collectionRef, { url: photoUri });
+        console.log('Photo saved to', collectionName, 'collection');
+        navigateToCollection(collectionName);
+      } catch (error) {
+        console.log('Error saving photo to', collectionName, 'collection:', error);
+      }
+    }
   };
 
-  const saveToCollection = () => {
-    navigation.navigate('Collection', { capturedPhoto });
-    resetPhoto();
+  const navigateToCollection = (collectionName) => {
+    navigation.navigate('Collection', { collection: collectionName });
   };
 
   if (hasPermission === null) {
@@ -77,74 +85,30 @@ export default function CameraScreen() {
 
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type} ref={cameraRef}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.flipButton} onPress={flipCamera}>
-            <Ionicons name="camera-reverse-outline" size={28} color="#fff" />
-          </TouchableOpacity>
-        </View>
-        {!capturedPhoto && (
-          <TouchableOpacity style={styles.captureButton} onPress={takePicture} />
-        )}
-      </Camera>
-      {capturedPhoto && (
-        <View style={styles.previewContainer}>
-          <TouchableOpacity style={styles.previewImageWrapper} onPress={resetPhoto}>
-            <Ionicons name="close-outline" size={28} color="#fff" />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.saveButton} onPress={showSaveConfirmation}>
-            <Ionicons name="save-outline" size={28} color="#fff" />
-          </TouchableOpacity>
-        </View>
-      )}
+      <Camera style={styles.camera} type={Camera.Constants.Type.back} ref={cameraRef} />
+      <TouchableOpacity style={styles.captureButton} onPress={takePicture} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#000',
   },
   camera: {
     flex: 1,
   },
-  buttonContainer: {
-    position: 'absolute',
-    top: 16,
-    left: 16,
-  },
-  flipButton: {
-    marginRight: 16,
-  },
   captureButton: {
-    position: 'absolute',
-    alignSelf: 'center',
-    marginBottom: 32,
     width: 80,
     height: 80,
     borderRadius: 40,
     borderWidth: 6,
     borderColor: '#fff',
-    backgroundColor: 'transparent',
-    bottom: 0,
-  },
-  previewContainer: {
     position: 'absolute',
-    bottom: 20,
-    left: 20,
-    right: 20,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  previewImageWrapper: {
-    padding: 10,
-    borderRadius: 20,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  saveButton: {
-    padding: 10,
-    borderRadius: 20,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    bottom: 40,
+    alignSelf: 'center',
   },
 });
+
+export default CameraScreen;
