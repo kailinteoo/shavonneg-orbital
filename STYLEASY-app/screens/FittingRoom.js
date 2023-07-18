fa9d7de9564da441b15bd8edc5416257519757cb
shavonneg <e0967851@u.nus.edu>
2023-07-18 15:51:57 +0800

    working fitting room

--- STYLEASY-app/screens/FittingRoom.js
+++ STYLEASY-app/screens/FittingRoom.js
@@ -1,37 +1,17 @@
 import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  FlatList,
-  Image,
-  TouchableOpacity,
-  Dimensions,
-  Alert,
-} from "react-native";
-import {
-  collection,
-  addDoc,
-  query,
-  getDocs,
-  deleteDoc,
-  doc,
-  setDoc,
-} from "firebase/firestore";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { collection, getDocs } from "firebase/firestore";
 import { auth, database } from "../config/firebase";
-import { useNavigation } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
-import * as ImagePicker from "expo-image-picker";
 
 const FittingRoom = () => {
-  const navigation = useNavigation();
+  const [topsImages, setTopsImages] = useState([]);
+  const [bottomsImages, setBottomsImages] = useState([]);
   const [shoesImages, setShoesImages] = useState([]);
-  const { width, height } = Dimensions.get("window");
-  const textSize = Math.round(width / 20); // Adjust the division factor as needed
-  const [randomImage, setRandomImage] = useState(null);
+  const [outfit, setOutfit] = useState({ top: null, bottom: null, shoes: null });
 
   useEffect(() => {
-    const fetchShoesImages = async () => {
+    const fetchImages = async (collectionName, setImages) => {
       try {
         const currentUser = auth.currentUser;
         if (currentUser) {
@@ -39,161 +19,51 @@ const FittingRoom = () => {
             database,
             "users",
             currentUser.uid,
-            "shoes"
+            collectionName
           );
           const userImagesSnapshot = await getDocs(userImagesCollectionRef);
-          const shoesImagesData = [];
+          const imagesData = [];
           userImagesSnapshot.forEach((doc) => {
-            shoesImagesData.push({ id: doc.id, url: doc.data().url });
+            imagesData.push({ id: doc.id, url: doc.data().url });
           });
-          setShoesImages(shoesImagesData);
+          setImages(imagesData);
         }
       } catch (error) {
-        console.log("Error fetching Shoes images:", error);
+        console.log(`Error fetching ${collectionName} images:`, error);
       }
     };
 
-    fetchShoesImages();
+    fetchImages("tops", setTopsImages);
+    fetchImages("bottoms", setBottomsImages);
+    fetchImages("shoes", setShoesImages);
   }, []);
 
-  const handleGoBack = () => {
-    navigation.navigate("Collection");
+  const handleGenerateOutfit = () => {
+    const randomTop = getRandomImage(topsImages);
+    const randomBottom = getRandomImage(bottomsImages);
+    const randomShoes = getRandomImage(shoesImages);
+
+    setOutfit({ top: randomTop, bottom: randomBottom, shoes: randomShoes });
   };
 
-  const handleSelectImage = async () => {
-    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (status !== "granted") {
-      alert("Permission to access the camera roll is required!");
-      return;
+  const getRandomImage = (images) => {
+    if (images.length > 0) {
+      const randomIndex = Math.floor(Math.random() * images.length);
+      return images[randomIndex].url;
     }
-
-    const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    if (!result.cancelled) {
-      const selectedImage = result.assets[0];
-      const currentUser = auth.currentUser;
-
-      if (currentUser) {
-        const userImagesCollectionRef = collection(
-          database,
-          "users",
-          currentUser.uid,
-          "shoes"
-        );
-        const imageRef = await addDoc(userImagesCollectionRef, {
-          url: selectedImage.uri,
-        });
-        setShoesImages((prevImages) => [
-          ...prevImages,
-          { id: imageRef.id, url: selectedImage.uri },
-        ]);
-      }
-    }
+    return null;
   };
-
-  const handleRemoveImage = async (imageId) => {
-    try {
-      const currentUser = auth.currentUser;
-      if (currentUser) {
-        const userImageDocRef = doc(
-          database,
-          "users",
-          currentUser.uid,
-          "shoes",
-          imageId
-        );
-        await deleteDoc(userImageDocRef);
-        setShoesImages((prevImages) =>
-          prevImages.filter((image) => image.id !== imageId)
-        );
-      }
-    } catch (error) {
-      console.log("Error removing image:", error);
-    }
-  };
-
-  const handleGenerateRandomImage = () => {
-    const randomIndex = Math.floor(Math.random() * shoesImages.length);
-    const randomImageUrl = shoesImages[randomIndex]?.url;
-    setRandomImage(randomImageUrl);
-  };
-
-  // Calculate the image size and gap between images based on the screen size
-  const imageSize = (width - 40) / 3; // Display three images per row
-  const gapSize = 10;
-  const firstRowGap = 100;
 
   return (
     <View style={styles.container}>
-      <Text style={[styles.headerText, { fontSize: textSize }]}>
-        FITTING ROOM
-      </Text>
-      <TouchableOpacity style={styles.backButton} onPress={handleGoBack}>
-        <Feather name="arrow-left" size={24} color="black" />
-      </TouchableOpacity>
-      <View style={styles.imageContainer}>
-        {randomImage ? (
-          <View style={styles.randomImageWrapper}>
-            <Image
-              source={{ uri: randomImage }}
-              style={styles.randomImage}
-            />
-          </View>
-        ) : (
-          <View style={styles.noRandomImageContainer}>
-            <Text style={styles.noRandomImageText}>
-              Click "Generate Random Image" to see a random picture
-            </Text>
-          </View>
-        )}
-        {shoesImages.length > 0 && (
-          <FlatList
-            data={shoesImages}
-            keyExtractor={(item) => item.id}
-            numColumns={3}
-            renderItem={({ item, index }) => (
-              <TouchableOpacity
-                style={[
-                  styles.imageWrapper,
-                  {
-                    width: imageSize,
-                    height: imageSize,
-                    marginTop: index < 3 ? firstRowGap : gapSize,
-                    marginLeft: index % 3 !== 0 ? gapSize : 0,
-                  },
-                ]}
-                onPress={() => handleRemoveImage(item.id)}
-              >
-                <Image source={{ uri: item.url }} style={styles.image} />
-                <TouchableOpacity
-                  style={styles.removeButton}
-                  onPress={() =>
-                    Alert.alert("Remove Image", "Are you sure you want to remove this image?", [
-                      { text: "Cancel", style: "cancel" },
-                      { text: "Remove", style: "destructive", onPress: () => handleRemoveImage(item.id) },
-                    ])
-                  }
-                >
-                  <Feather name="trash-2" size={18} color="white" />
-                </TouchableOpacity>
-              </TouchableOpacity>
-            )}
-          />
-        )}
+      <Text style={styles.headerText}>FITTING ROOM</Text>
+      <View style={styles.outfitContainer}>
+        <Image source={{ uri: outfit.top }} style={styles.outfitImage} />
+        <Image source={{ uri: outfit.bottom }} style={styles.outfitImage} />
+        <Image source={{ uri: outfit.shoes }} style={styles.outfitImage} />
       </View>
-      <TouchableOpacity style={styles.uploadButton} onPress={handleSelectImage}>
-        <Text style={styles.uploadButtonText}>Upload Image</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.generateButton}
-        onPress={handleGenerateRandomImage}
-      >
-        <Text style={styles.generateButtonText}>Generate Random Image</Text>
+      <TouchableOpacity style={styles.generateButton} onPress={handleGenerateOutfit}>
+        <Text style={styles.generateButtonText}>Generate Outfit</Text>
       </TouchableOpacity>
     </View>
   );
@@ -208,92 +78,28 @@ const styles = StyleSheet.create({
   },
   headerText: {
     fontWeight: "bold",
-    marginTop: 70,
-    marginBottom: -50,
-    textAlign: "center",
+    fontSize: 20,
+    marginBottom: 20,
   },
-  backButton: {
-    position: "absolute",
-    top: 70,
-    left: 10,
-    padding: 10,
+  outfitContainer: {
+    alignItems: "center",
+    marginBottom: 20,
   },
-  uploadButton: {
-    position: "absolute",
-    bottom: 20,
-    right: 20,
-    padding: 10,
-    backgroundColor: "#7b68ee",
-    borderRadius: 10,
-  },
-  uploadButtonText: {
-    color: "#ffffff",
-    fontWeight: "bold",
-    fontSize: 14,
+  outfitImage: {
+    width: 200,
+    height: 200,
+    resizeMode: "cover",
+    marginVertical: 10,
   },
   generateButton: {
-    position: "absolute",
-    bottom: 20,
-    left: 20,
-    padding: 10,
     backgroundColor: "#7b68ee",
+    padding: 10,
     borderRadius: 10,
   },
   generateButtonText: {
     color: "#ffffff",
     fontWeight: "bold",
-    fontSize: 14,
-  },
-  imageContainer: {
-    flex: 1,
-    alignItems: "center",
-  },
-  imageWrapper: {
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 8,
-    overflow: "hidden",
-  },
-  image: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "cover",
-  },
-  randomImageWrapper: {
-    width: 200,
-    height: 200,
-    borderRadius: 8,
-    overflow: "hidden",
-    marginBottom: 20,
-  },
-  randomImage: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "cover",
-  },
-  removeButton: {
-    position: "absolute",
-    top: 5,
-    right: 5,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    borderRadius: 10,
-    padding: 5,
-  },
-  noImagesContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  noImagesText: {
-    fontSize: 18,
-  },
-  noRandomImageContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  noRandomImageText: {
-    fontSize: 18,
+    fontSize: 16,
   },
 });
 
